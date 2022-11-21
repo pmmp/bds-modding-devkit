031ee0d93c51318ef08137696041b080176042b0
Dylan K. Taylor <dktapps@pmmp.io>
2022-11-21 14:35:50 +0000

    Split helper.sh into individual scripts to allow auto-completing

--- helper.sh
+++ helper.sh
@@ -6,11 +6,6 @@ LY=$(echo -en '\033[01;33m')
 LC=$(echo -en '\033[01;36m')
 W=$(echo -en '\033[01;37m')
 
-LIBS="$PWD/mods"
-MODS_CODE="$PWD/code"
-SDK="$PWD/sdk"
-CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=\"$SDK\""
-
 show_help(){
 	echo "${LC}Usage: $LG$0 ${LR}command ${LC}<${LY}arguments${LC}>
 
@@ -23,183 +18,27 @@ ${LG}Available commands:
   ${LR}build ${LC}<${LY}all${LC}|${LY}name${LC}>${W} — ${LC}builds all mods or selected$R"
 }
 
-download(){
-	curl --insecure --silent --show-error --location --globoff $1 > $2
-	if [[ "$3" = true ]]; then
-		unzip -qq $2 && rm $2 || exit 1
-	fi
-}
-
-get_latest_release(){
-  curl -s "https://api.github.com/repos/$1/releases/latest" | grep '"browser_download_url":.'$2 | sed -E 's/.*"([^"]+)".*/\1/'
-}
-
-check_args(){
-	if [ "$1" -ne $2 ]; then
-		show_help && exit 0
-	fi
-}
-
-setup_sdk(){
-	local working_directory="$1"
-	cd "$working_directory"
-	rm -rf sdk && mkdir -p sdk && cd sdk
-	download $(get_latest_release "minecraft-linux/server-modloader" "*\.zip") mod_sdk.zip true
-	cd ..
-}
-
-build_mod(){
-	local mod_path="$1"
-	local cmake_flags="$2"
-	if [[ -f "$1/CMakeLists.txt" ]]; then
-		cd "$mod_path"
-
-		mkdir -p build && cd build
-		cmake $cmake_flags .. || exit 1
-		make -j2 || exit 1
-
-		cp *.so $LIBS
-	else
-		echo "${LR} Could not find $mod_path"
-	fi
-}
-
-clean_old_server(){
-	local working_directory="$1"
-	cd "$working_directory"
-	rm -rf structures behavior_packs resource_packs definitions || true
-	rm bedrock_server bedrock_server_symbols.debug bedrock_server_symbols_test.debug || true
-	echo "${LG} > Old server files removed from ${working_directory}$R"
-}
-
-export_server_symbols(){
-	local input_path="$1"
-	local output_path="$2"
-
-	echo "${LY} > Copying server binary"
-	cp "$input_path" "$output_path"
-
-	echo "${LY} > Stripping DWARF if present"
-	strip --strip-debug "$output_path"
-
-	echo "${LY} > Converting symbols"
-	python3 export-symbols.py "$output_path" "$output_path"
-}
-
-copy_server_files(){
-	local working_directory="$2"
-	local server_files="$1"
-
-	echo "${LY} > Installing new server files from $server_files (this might take a few minutes)${R}"
-	cp -r "$server_files/structures" "$working_directory"
-	cp -r "$server_files/behavior_packs" "$working_directory"
-	cp -r "$server_files/resource_packs" "$working_directory"
-	cp -r "$server_files/definitions" "$working_directory"
-	cp "$server_files/bedrock_server" "$working_directory"
-	cp "$server_files/bedrock_server_symbols.debug" "$working_directory"
-
-	if [ ! -f "$working_directory/server.properties" ]; then
-		echo "${LY} > Creating server.properties$R"
-		cp "$server_files/server.properties" "$working_directory"
-	else
-		echo "${LY} > server.properties already exists$R"
-	fi
-
-	echo "${LG} > New server files installed!"
-}
-
-install_server(){
-	local server_files="$1"
-	local working_directory="$2"
-
-	clean_old_server "$working_directory"
-
-	copy_server_files "$server_files" "$working_directory"
-
-	export_server_symbols "$working_directory/bedrock_server_symbols.debug" "$working_directory/bedrock_server_symbols_test.debug"
-
-	echo "${LG} > Installation successful."
-	echo "${LG} > Don't forget to build mods before running the server: ${LY}$0 build all$R"
-}
-
-install_modloader(){
-	local working_directory="$1"
-	setup_sdk "$working_directory"
-	mkdir -p "$working_directory/mods/"
-
-	echo "${LG} > modloader-sdk downloaded!$R"
-	echo "${LY} > modloader-sdk saved in $working_directory$R"
-}
-
-build_all_mods(){
-	local mods_code="$1"
-	local cmake_flags="$2"
-	export -f build_mod && export LIBS=$LIBS
-	find "$mods_code" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -n1 -P4 -I '@' bash -c "build_mod @ \"$cmake_flags\" || exit 255" || exit 1
-}
-
-
 case $1 in
 	"setup")
-		check_args $# 2
-
-		install_modloader "$PWD"
-		install_server "$2" "$PWD"
-		build_all_mods "$MODS_CODE" "$CMAKE_FLAGS"
-
-		echo "${LY}>>> Setup successful. Try ${LG}./start.sh ${LY}<<<$R"
+		./scripts/setup "$@"
 		;;
 	"clean-server")
-		clean_old_server "$PWD"
+		./scripts/clean-server "$@"
 		;;
 	"install-server")
-		check_args $# 2
-
-		install_server "$2" "$PWD"
+		./scripts/install-server "$@"
 		;;
 	"export-server-symbols")
-		export_server_symbols "$PWD/bedrock_server_symbols.debug" "$PWD/bedrock_server_symbols_test.debug"
+		./scripts/export-server-symbols "$@"
 		;;
 	"install-modloader")
-		install_modloader "$PWD"
-
-		echo "${LY}>>> Installation successful. Try ${LG}./start.sh ${LY}<<<$R"
+		./scripts/install-modloader "$@"
 		;;
 	"gen")
-		check_args $# 2
-
-		mkdir -p "$MODS_CODE/$2/src" && cd $MODS_CODE
-		echo "cmake_minimum_required(VERSION 3.0)
-project($2)
-
-set(CMAKE_PREFIX_PATH ../../sdk)
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-
-find_package(ModLoader REQUIRED)
-
-add_library($2 SHARED src/main.cpp)
-target_link_libraries($2 PUBLIC ModLoader)" > "$MODS_CODE/$2/CMakeLists.txt"
-		echo "#include <modloader/log.h>
-#include <modloader/statichook.h>
-
-using namespace modloader;
-
-#define TAG \"$2\"
-
-extern \"C\" void modloader_on_server_start(void* serverInstance) {
-	Log::verbose(TAG, \"Hello, world!\");
-}" > "$MODS_CODE/$2/src/main.cpp"
+		./scripts/gen "$@"
 		;;
 	"build")
-		check_args $# 2
-
-		if [[ "$2" == "all" ]]; then
-			build_all_mods "$MODS_CODE" "$CMAKE_FLAGS"
-		else
-			build_mod $2 "$CMAKE_FLAGS"
-		fi
-		echo "${LG} > All mods built successfully. To start the server with mods loaded, run ${LY}./start.sh$R"
+		./scripts/build "$@"
 		;;
 	*)
 		show_help
