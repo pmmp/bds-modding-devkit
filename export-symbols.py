c91691768aac5342c5adfe0b39fd36c76cd0c44d
Dylan K. Taylor <dktapps@pmmp.io>
2023-11-08 13:09:09 +0000

    Make local symbols global again for 1.20.50 modding

--- export-symbols.py
+++ export-symbols.py
@@ -7,5 +7,8 @@ if len(sys.argv) < 3:
 
 lib_symbols = lief.parse(sys.argv[1])
 for s in filter(lambda e: e.exported, lib_symbols.static_symbols):
+    #only modify local symbols to minimize scope
+    if s.binding == lief.ELF.SYMBOL_BINDINGS.LOCAL:
+        s.binding = lief.ELF.SYMBOL_BINDINGS.GLOBAL
     lib_symbols.add_dynamic_symbol(s)
 lib_symbols.write(sys.argv[2])
