20820452da58f0c050f9c45b5f7851e463355984
Dylan K. Taylor <dktapps@pmmp.io>
2024-08-01 14:50:29 +0000

    Duct tape for newer internal BDS which have embedded symbols again

--- export-symbols.py
+++ export-symbols.py
@@ -5,7 +5,12 @@ if len(sys.argv) < 3:
     print("Required arguments: input path, output path")
     exit(1)
 
+
 lib_symbols = lief.parse(sys.argv[1])
+if lib_symbols is None:
+    print("Error opening given BDS file, aborting")
+    exit(1)
+
 for s in lib_symbols.static_symbols:
     #only modify local symbols to minimize scope
     s.visibility = lief.ELF.SYMBOL_VISIBILITY.DEFAULT
