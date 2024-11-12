9c667894478fc93bc548f25c1e8e76f50cac5d09
Dylan K. Taylor <dktapps@pmmp.io>
2024-11-12 21:59:54 +0000

    Update export-symbols.py for lief 0.15

--- export-symbols.py
+++ export-symbols.py
@@ -11,10 +11,10 @@ if lib_symbols is None:
     print("Error opening given BDS file, aborting")
     exit(1)
 
-for s in lib_symbols.static_symbols:
+for s in lib_symbols.symtab_symbols:
     #only modify local symbols to minimize scope
-    s.visibility = lief.ELF.SYMBOL_VISIBILITY.DEFAULT
-    if s.binding == lief.ELF.SYMBOL_BINDINGS.LOCAL:
-        s.binding = lief.ELF.SYMBOL_BINDINGS.GLOBAL
+    s.visibility = lief.ELF.Symbol.VISIBILITY.DEFAULT
+    if s.binding == lief.ELF.Symbol.BINDING.LOCAL:
+        s.binding = lief.ELF.Symbol.BINDING.GLOBAL
     lib_symbols.add_dynamic_symbol(s)
 lib_symbols.write(sys.argv[2])
