7f5289f28292f1da8a88d726402ce3cdad40baf9
Dylan K. Taylor <dktapps@pmmp.io>
2025-09-20 15:48:05 +0100

    Be more selective in symbol exporting this avoids conflicts if libraries may have been statically compiled into BDS, but were also used by dynamically linked system libraries.
    NB: This might break again in the future in the event of C++ dynamically linked system libs, or if
    we need to access non-mangled symbols from Minecraft itself. In those cases we might just have to
    manually whitelist/blacklist certain symbols.

--- export-symbols.py
+++ export-symbols.py
@@ -12,9 +12,15 @@ if lib_symbols is None:
     exit(1)
 
 for s in lib_symbols.symtab_symbols:
-    #only modify local symbols to minimize scope
-    s.visibility = lief.ELF.Symbol.VISIBILITY.DEFAULT
-    if s.binding == lief.ELF.Symbol.BINDING.LOCAL:
-        s.binding = lief.ELF.Symbol.BINDING.GLOBAL
-    lib_symbols.add_dynamic_symbol(s)
+    #Only export C++ mangled symbols - this is to avoid symbol conflicts with the likes of OpenSSL
+    #They seem to compile OpenSSL statically, but dynamically link to system cURL which may use
+    #a different, incompatible OpenSSL, resulting in wrong symbol resolving and crashes
+    #this is the easiest workaround for now, but it might be a problem again in the future, or
+    #if we need to access any non-mangled symbols from Minecraft (which aren't namespaced, so we
+    #can't easily distinguish them from other libraries...)
+    if not s.imported and (s.is_function or s.is_variable) and s.name.startswith('_Z'):
+        s.visibility = lief.ELF.Symbol.VISIBILITY.DEFAULT
+        if s.binding == lief.ELF.Symbol.BINDING.LOCAL:
+            s.binding = lief.ELF.Symbol.BINDING.GLOBAL
+        lib_symbols.add_dynamic_symbol(s)
 lib_symbols.write(sys.argv[2])
